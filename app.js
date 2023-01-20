4c3dbfb70dc9d99bc597c60fe26c0d375b123d87
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-20 12:41:19 -0800

    adds getBeanieBabies() function. on window load, fetches data to console

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 /* Imports */
-
+import { getBeanieBabies } from './fetch-utils.js';
 /* Get DOM Elements */
 
 /* State */
+let beanieBabies = [];
 
 /* Events */
-
+window.addEventListener('load', async () => {
+    const response = await getBeanieBabies();
+    beanieBabies = response;
+    console.log(response);
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
